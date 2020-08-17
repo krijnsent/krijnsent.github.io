72fa63c431cb76b117f47b611f726e60cefffda8
Koen Rijnsent <koen.rijnsent@gmail.com>
2020-08-17 16:00:27 +0200

    Switched to PHP, added GET and 8 more murals

--- muralsonamap/index.php
+++ muralsonamap/index.php
@@ -7,12 +7,14 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+    <link href="css/bootstrap.min.css" rel="stylesheet"> <!--load all fontawesome styles -->
     <link href="css/all.min.css" rel="stylesheet"> <!--load all fontawesome styles -->
     <link href="css/leaflet.css" rel="stylesheet" />
     <link href="css/leaflet-sidebar.css" rel="stylesheet"> <!--load custom styles -->
     <link href="css/murals.css" rel="stylesheet"> <!--load custom styles -->
 
     <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/leaflet.js"></script>
     <script src="js/leaflet-sidebar.min.js"></script>
 
@@ -40,7 +42,6 @@
     <div class="sidebar-tabs">
         <ul role="tablist">
             <li><a href="#home" role="tab"><i class="fa fa-bars"></i></a></li>
-            <li><a href="#filter" role="tab"><i class="fa fa-filter"></i></a></li>
             <li><a href="#about" role="tab"><i class="fa fa-question-circle"></i></a></li>
         </ul>
     </div>
@@ -52,20 +53,26 @@
                 Murals on a map
                 <span class="sidebar-close"><i class="fa fa-caret-left"></i></span>
             </h1>
-            <p>A hobby project to map out murals in Utrecht, NL. The images I use are of poor quality, I scale them to max 300px.
-                That is on purpose: the best way to enjoy these murals is to be in front of them.
-                So enjoy this site as your guide for a nice bike ride :).</p>
-        </div>
+            <p>A hobby project to map out murals in Utrecht, NL.</p>
+            <p>Please leave feedback/suggestions at <a href="https://www.reddit.com/r/Utrecht/comments/i9i7hc/murals_in_utrecht/" target="_blank">my Reddit post</a>
+                or as an issue at <a href="https://github.com/krijnsent/krijnsent.github.io" target="_blank">the Github repo</a>.</p>
 
-        <div class="sidebar-pane" id="filter">
-            <h1 class="sidebar-header">Highlight<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-            <p>Highlight murals by artist or by title</p>
-            Work in progress...
+            <h4>Highlight murals by artist or by title</h4>
+            <form class="form-horizontal">
+                <div class="input-group">
+                    <input type="text" id="highlight_filter" class="form-control" placeholder="Highlight with text" oninput="update_highlight();" aria-label="Highlight with text" aria-describedby="basic-addon2" value='<?php echo htmlspecialchars($_GET["q"]); ?>' >
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-danger" type="button" onclick="clear_highlight_filter();" >X</button>
+                    </div>
+                </div>
+            </form>
         </div>
 
         <div class="sidebar-pane" id="about">
             <h1 class="sidebar-header">About<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-            Please leave feedback/suggestions at <a href="https://www.reddit.com/r/Utrecht/comments/i9i7hc/murals_in_utrecht/" target="_blank">my Reddit post</a>
+            The images I use are of poor quality, I scale them to max 300px.
+            That is on purpose: the best way to enjoy these murals is to be in front of them.
+            So enjoy this site as your guide for a nice bike ride :). Please leave feedback/suggestions at <a href="https://www.reddit.com/r/Utrecht/comments/i9i7hc/murals_in_utrecht/" target="_blank">my Reddit post</a>
             or as an issue at <a href="https://github.com/krijnsent/krijnsent.github.io" target="_blank">the Github repo</a>.
             <h2>Sources</h2>
             <ul>
@@ -89,15 +96,33 @@
 
     var m_map;
     var murals_data = 1;
-    var murals_markers = new L.FeatureGroup();
+    //var murals_markers = new L.FeatureGroup();
+    var murals_layer_group = new L.LayerGroup();
+    var sett_filter = document.getElementById("highlight_filter");
 
     var IconMural = function(latlng, properties) {
         var bg_color = "#FFFFFF";
+        var w = 2;
+        var filter_by = sett_filter.value.toLowerCase();
+
+        if (filter_by === "") {
+            //no different settings
+        } else {
+            //console.log("FILTER");
+            var nn = properties.name.toLowerCase().indexOf(filter_by);
+            var na = properties.artist.toLowerCase().indexOf(filter_by);
+            if (nn >= 0 || na >= 0) {
+                w = 4;
+                bg_color = "#FFFF00";
+            } else {
+                // no different settings
+            }
+        }
         return L.circleMarker(latlng, {
             radius: 8,
             fillColor: bg_color,
+            weight: w,
             color: "#000000",
-            weight: 3,
             opacity: 1,
             fillOpacity: 0.6
         })
@@ -117,6 +142,7 @@
         m_map.attributionControl.setPrefix('');
         m_map.setView(UTRECHT_DOM, 13);
 
+        murals_layer_group.addTo(m_map);
         load_murals_data();
 
         var sidebar = L.control.sidebar('sidebar').addTo(m_map);
@@ -131,7 +157,7 @@
                 //console.log("MURALS DATA RESPONSE OK");
                 //console.log(response);
                 murals_data = response;
-                draw_murals();
+                redraw_murals();
             },
             error: function(response){
                 console.log("ERROR MURALS LOADING");
@@ -140,9 +166,9 @@
         });
     }
 
-    function draw_murals() {
+    function redraw_murals() {
         console.log("START DRAW MURALS");
-        murals_markers.clearLayers();
+        murals_layer_group.clearLayers();
         //L.geoJSON(murals_data).addTo(murals_markers);
 
         //loop through murals_data
@@ -156,22 +182,24 @@
                 var popup_txt = '<b>Titel</b>: ' + feature.properties.name + '<br/>' ;
                 popup_txt +=  '<b>Artiest</b>: ' + feature.properties.artist + '<br/>';
                 popup_txt +=  '<img src="img/' + feature.properties.image + '" alt="Foto"/>';
-
-                //var mural_icon = IconMural(mural_pos).bindPopup(popup_txt);
                 layer.bindPopup(popup_txt, {minWidth : 300});
-
-                //murals_markers.addLayer(mural_icon);
-                //console.log("add");
-                //murals_markers.addLayer(L.marker([feature.geometry.coordinates[0],feature.geometry.coordinates[1]]));
-                //console.log("2");
-                //layer.bindPopup(feature.properties.name);
             }
         });
 
-        geojson.addTo(m_map);
-        //console.log(murals_markers);
-        //m_map.addLayer(murals_markers);
+        murals_layer_group.addLayer(geojson);
     }
+
+    function update_highlight(){
+        console.log("UPDATE_FILTER");
+        redraw_murals()
+    }
+
+    function clear_highlight_filter(){
+        console.log("CLEAR_FILTER");
+        sett_filter.value = "";
+        redraw_murals();
+    }
+
 
 </script>
 
